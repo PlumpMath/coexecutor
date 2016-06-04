0703f0c7a67483cd165efb3f97a94aa97bfcbbad
Keunhong Lee <dlrmsghd@gmail.com>
2016-06-04 21:48:34 +0900

    map works

--- coexecutor/coroutine.py
+++ coexecutor/coroutine.py
@@ -2,7 +2,6 @@ from coexecutor._base import *
 import asyncio
 from collections import deque
 
-
 class CoroutinePoolExecutor(CoExecutor):
     def __init__(self, max_workers: int = 1, loop: asyncio.AbstractEventLoop = None, debug=False):
         if not max_workers or not isinstance(max_workers, int) or max_workers < 1:
@@ -35,6 +34,29 @@ class CoroutinePoolExecutor(CoExecutor):
 
             self._futures_to_wait["thread_monitor"] = asyncio.ensure_future(_thread_monitor(), loop=self._loop)
 
+    async def _launcher(self, coro):
+        ret = await coro
+        self._current_workers -= 1
+        while self._current_workers < self._max_workers:
+            waiting_future = None
+            while len(self._pending_queue) > 0:
+                generator = self._pending_queue[0]
+                try:
+                    waiting_future = next(generator)
+                    break
+                except StopIteration:
+                    self._pending_queue.popleft()
+                    continue
+            if waiting_future is None:
+                break
+
+            self._current_workers += 1
+            asyncio.ensure_future(waiting_future, loop=self._loop)
+        if self._idle():
+            async with self._waiter:
+                self._waiter.notify_all()
+        return ret
+
     @overrides
     def submit(self, coroutine_function, *args, **kwargs) -> asyncio.Future:
         if coroutine_function is None or not asyncio.iscoroutinefunction(coroutine_function):
@@ -42,41 +64,87 @@ class CoroutinePoolExecutor(CoExecutor):
         if self._shutdowned:
             raise RuntimeError("Executor is closed.")
 
-        async def _launcher():
-            if "loop" in kwargs:
-                if kwargs["loop"] is not self._loop:
-                    raise RuntimeError("Loop mismatch. Definitely a bug.")
-            else:
-                kwargs["loop"] = self._loop
-            ret = await coroutine_function(*args, **kwargs)
-            self._current_workers -= 1
-            while len(self._pending_queue) > 0 and self._current_workers < self._max_workers:
-                waiting_future = self._pending_queue.popleft()
-                self._current_workers += 1
-                asyncio.ensure_future(waiting_future, loop=self._loop)
-            if self._idle():
-                async with self._waiter:
-                    self._waiter.notify_all()
-            return ret
-        future = _launcher()
+        if "loop" in kwargs:
+            if kwargs["loop"] is not self._loop:
+                raise RuntimeError("Loop mismatch. Definitely a bug.")
+        else:
+            kwargs["loop"] = self._loop
+
+        future = self._launcher(coroutine_function(*args, **kwargs))
 
         if self._current_workers < self._max_workers:
             self._current_workers += 1
             asyncio.ensure_future(future, loop=self._loop)
         else:
-            self._pending_queue.append(future)
+            def _single_generator():
+                yield future
+            self._pending_queue.append(_single_generator())
         return future
 
     def _idle(self):
         return self._current_workers == 0 and len(self._pending_queue) == 0
 
+    class _async_generator():
+        def __init__(self, loop, limit=None):
+            if limit is None:
+                limit = 0
+            self._queue = asyncio.Queue(maxsize=limit, loop=loop)
+            self._stopped = False
+            self._waiter = None
+
+        async def put(self, item):
+            await self._queue.put(item)
+
+        def stop(self):
+            if not self._stopped:
+                self._stopped = True
+                if self._queue.empty() and self._waiter is not None:
+                    self._waiter.cancel()
+
+        async def __aiter__(self):
+            return self
+
+        async def __anext__(self):
+            if self._stopped and self._queue.empty():
+                raise StopAsyncIteration
+            future = self._queue.get()
+            self._waiter = future
+            try:
+                return await future
+            except asyncio.CancelledError:
+                self._waiter = None
+                raise StopAsyncIteration
+
+
     @overrides
-    def map(self, coroutine_function, *iterables, timeout=None):
+    def map(self, coroutine_function, *iterables, limit=None):
         if coroutine_function is None or not asyncio.iscoroutinefunction(coroutine_function):
             raise TypeError("Must provide a coroutine function")
         if self._shutdowned:
             raise RuntimeError("Executor is closed.")
-        raise NotImplementedError()
+
+        queue = self._async_generator(loop=self._loop, limit=limit)
+
+        def _coroutine_generator():
+            async def _wrapper(arg):
+                ret = await coroutine_function(*arg, loop=self._loop)
+                await queue.put(ret)
+            for arg in zip(*iterables):
+                yield self._launcher(_wrapper(arg))
+            queue.stop()
+
+        generator = _coroutine_generator()
+
+        while self._current_workers < self._max_workers:
+            self._current_workers += 1
+            try:
+                current_coro = next(generator)
+                asyncio.ensure_future(current_coro, loop=self._loop)
+            except StopIteration:
+                return queue
+
+        self._pending_queue.append(generator)
+        return queue
 
     @overrides
     async def shutdown(self, wait=True):
@@ -98,40 +166,22 @@ class CoroutinePoolExecutor(CoExecutor):
 
 
 def main():
-    async def test(loop):
+    async def test(index, index2, loop):
         await asyncio.sleep(1, loop=loop)
-        print("hello")
+        print("test", index, index2)
+        return (index,index2)
 
 
     loop = asyncio.new_event_loop()
-    coex = CoroutinePoolExecutor(loop=loop, debug=True)
-    coex.submit(test)
-    coex.submit(test)
-    coex.submit(test)
-    coex.submit(test)
 
     async def async_main(loop):
-        print("ha1")
-        await coex.shutdown(wait=False)
-        await asyncio.sleep(3, loop=loop)
-        print("ha2")
+        coex = CoroutinePoolExecutor(loop=loop, max_workers=4, debug=True)
+        async for ret in coex.map(test, range(10), range(10,20)):
+            print(ret)
         await coex.shutdown(wait=True)
-        print("ha3")
-
-    async def async_main2(loop):
-        print("ho1")
-        await asyncio.sleep(2, loop=loop)
-        await coex.shutdown(wait=True)
-        await coex.shutdown(wait=False)
-        print("ho2")
-        await coex.shutdown(wait=True)
-        print("ho3")
 
     try:
-        #loop.run_until_complete(async_main(loop=loop))
-        asyncio.ensure_future(async_main(loop=loop), loop=loop)
-        asyncio.ensure_future(async_main2(loop=loop), loop=loop)
-        loop.run_forever()
+        loop.run_until_complete(async_main(loop=loop))
     finally:
         loop.close()
 
