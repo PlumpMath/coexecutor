16f18ef2dd900d19e9f649a3cbb5c470509236f5
Keunhong Lee <dlrmsghd@gmail.com>
2016-06-06 01:57:44 +0900

    may work..

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from distutils.core import setup
-from setuptools import find_packages
 
 setup(
     version='0.01',
     name="coexecutor",
     author="Keunhong Lee",
     license="GNU LESSER GENERAL PUBLIC LICENSE VERSION 3",
-    packages=find_packages(),
+    packages=["coexecutor"],
 )
