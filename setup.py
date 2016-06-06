e521ec8b539913b3fab70f104e3d28ab53154699
Keunhong Lee <dlrmsghd@gmail.com>
2016-06-06 14:46:27 +0900

    why there is a comma;

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ classifiers=[
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
     "Programming Language :: Python :: 3.5",
-],
+]
 
 setup(
     name="coexecutor",
