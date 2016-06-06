892eb0469138f1f5edef4fb5a1d5030d5d9609a6
Keunhong Lee <dlrmsghd@gmail.com>
2016-06-06 14:57:38 +0900

    try adding python3.6 support

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ CoExecutor constructor requires a loop to rely on.
 
 With debug=True, it will print the number of active coroutines per second.
 
-Works with Python3.5 only.
+Works with Python3.5+.
 """
 
 classifiers=[
@@ -57,6 +57,7 @@ classifiers=[
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
     "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
 ]
 
 setup(
